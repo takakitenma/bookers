4b92bfe18265f4a7075cdd5246080de86df3289f
takakitenma <tenma1507@gmail.com>
2023-06-03 13:44:09 +0000

    [bookers]books_controllerまで作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "6c9fd3c320a74e47ab8f07a3dbe1b04b.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
